1003b6379b276e2bb1cbb54b4a792ed1bd134639
Riccardo <richi-97@live.it>
2025-12-09 11:47:38 +0100

    profile page and header setup

---
+++ linked-in/src/components/HeaderProfile.jsx
@@ -0,0 +1,7 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const HeaderProfile = function () {
+  return <Row></Row>;
+};
+
+export default HeaderProfile;
---
+++ linked-in/src/components/ProfilePage.jsx
@@ -0,0 +1,16 @@
+import { Container } from "react-bootstrap";
+
+const ProfilePage = function () {
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} md={9}>
+          {/* HEADERPROFILE */}
+        </Col>
+        <Col xs={12} md={3}>
+          {/* ASIDE con Lingua del Profilo Potrebbero interessarti etc.  */}
+        </Col>
+      </Row>
+    </Container>
+  );
+};
