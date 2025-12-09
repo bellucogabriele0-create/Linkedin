45ff445fca361861a3f8447bd8c6de600df52075
Riccardo <richi-97@live.it>
2025-12-09 12:09:12 +0100

    mounted ProfilePage on App and ProfileHeader on ProfilePage

--- linked-in/src/App.jsx
+++ linked-in/src/App.jsx
@@ -3,13 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <Provider store={store}>
       <BrowserRouter>
         {/* NAV */}
-        <Routes>{/* <Route> con profile page etc */}</Routes>
+        <Routes>
+          <Route path="/" element={<ProfilePage />} />
+        </Routes>
         {/* FOOTER */}
       </BrowserRouter>
     </Provider>
--- linked-in/src/components/HeaderProfile.jsx
+++ linked-in/src/components/HeaderProfile.jsx
@@ -1,7 +1,24 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
 
 const HeaderProfile = function () {
-  return <Row></Row>;
+  return (
+    <Row>
+      <Col xs={12}>
+        <Card className="rounded-3">
+          <Card.Img variant="top" src="https://placecats.com/800/300" />
+          <Card.Body>
+            <Card.Title>Nome Cognome</Card.Title>
+            <Card.Text>BIO </Card.Text>
+            <Button variant="primary btn-rounded-pill">Disponibile per</Button>
+            <Button variant="primary btn-rounded-pill">Aggiungi sezione</Button>
+            <Button variant="primary btn-rounded-pill">Migliora profilo</Button>
+            <Button variant="primary btn-rounded-pill">ICONA 3 puntini</Button>
+            <Row></Row>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default HeaderProfile;
