190e0a31d2172e64af513928219c9adf35e54334
Riccardo <richi-97@live.it>
2025-12-09 11:08:23 +0100

    initial setup

---
+++ linked-in/src/redux/reducers/profileReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  content: {},
+};
+const profileReducer = function (currentState = initialState, action) {
+  switch (action.type) {
+    // cases
+    default:
+      return currentState;
+  }
+};
+
+export default profileReducer;
