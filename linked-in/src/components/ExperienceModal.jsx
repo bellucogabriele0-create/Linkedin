71c5e2ceb242f28b9c24dba4961c735c2d767368
KeviN-XVII <kevinquartar@gmail.com>
2025-12-10 14:53:12 +0100

    sistemato modale

--- linked-in/src/components/ExperienceModal.jsx
+++ linked-in/src/components/ExperienceModal.jsx
@@ -2,25 +2,23 @@ import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import ExperienceForm from "./ExperienceForm";
 
-const ExperienceModal = function () {
+const ExperienceModal = function ({ close }) {
   return (
-    <div
-      className="modal show"
-      style={{ display: "block", position: "initial" }}
-    >
-      <Modal.Dialog>
-        <Modal.Header closeButton>
-          <Modal.Title>Modifica Esperienza</Modal.Title>
-        </Modal.Header>
+    <Modal show onHide={close}>
+      <Modal.Header closeButton>
+        <Modal.Title>Modifica Esperienza</Modal.Title>
+      </Modal.Header>
 
-        <Modal.Body>
-          <ExperienceForm />
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary">Close</Button>
-        </Modal.Footer>
-      </Modal.Dialog>
-    </div>
+      <Modal.Body>
+        <ExperienceForm />
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={close}>
+          Close
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 };
 
