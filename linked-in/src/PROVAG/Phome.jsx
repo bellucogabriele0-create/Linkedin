fa29e28ca233811de631b74955418718a54e25db
DiLorLorenzo <dilorenzoshark97@gmail.com>
2025-12-11 09:02:24 +0100

    Update Phome.jsx

--- linked-in/src/PROVAG/Phome.jsx
+++ linked-in/src/PROVAG/Phome.jsx
@@ -1,30 +1,27 @@
 import { Container, Col, Row } from "react-bootstrap";
-// import Competenze from "./Competenze";
-// import Formazione from "./Formazione";
-// import HeaderProfile from "./HeaderProfile";
-// import AnalisiAttivita from "./AnalisiAttivita";
 import { useDispatch } from "react-redux";
 import { getProfile } from "../redux/actions";
 import { useEffect } from "react";
-// import Esperienze from "./Esperienze";
 import Paggiungi from "./Paggiungi";
 import Pamici from "./Pamici";
 import Ppost from "./Ppost";
 import RightSidebar from "../components/RightSidebar";
 import LeftSidebar from "../components/LeftSidebar";
+
 const Phome = function () {
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getProfile());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+
   return (
     <Container>
       <Row>
-        <Col xs={12} md={3}>
+        <Col lg={3} className="d-none d-lg-block">
           <LeftSidebar />
         </Col>
-        <Col xs={12} md={6}>
+
+        <Col xs={12} lg={6}>
           <Paggiungi />
           <p>
             ---------------------------------------------------- seleziona la
@@ -34,7 +31,8 @@ const Phome = function () {
           <Pamici />
           <Ppost />
         </Col>
-        <Col xs={12} md={3}>
+
+        <Col lg={3} className="d-none d-lg-block">
           <RightSidebar />
         </Col>
       </Row>
