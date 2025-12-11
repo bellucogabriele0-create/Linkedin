ed4c3320767a080a47941937e479baf1fd9d5fcc
bellucogabriele0-create <bellucogabriele0@gmail.com>
2025-12-11 13:03:50 +0100

    profili amici

--- linked-in/src/App.jsx
+++ linked-in/src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         {/* NAV */}
         <Routes>
           <Route path="/ProfilePage" element={<ProfilePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
           <Route path="/" element={<Phome />} />
         </Routes>
         {/* FOOTER */}
